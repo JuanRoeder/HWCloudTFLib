a259a54066e9463966807e7abd2e38bad060c784
JuanRoeder <juan.roeder@gmail.com>
2024-11-20 10:05:08 -0500

    Se agregó y movió los output al archivo ouputs.tf

--- locals.tf
+++ locals.tf
@@ -98,23 +98,3 @@ locals {
     }
   }
 }
-
-output "enum_disk_type" {
-  value = local.enum_disk_type
-}
-
-output "enum_image_type" {
-  value = local.enum_image_type
-}
-
-output "enum_images" {
-  value = local.enum_images
-}
-
-output "enum_performance_type" {
-  value = local.enum_performance_type
-}
-
-output "enum_regions" {
-  value = local.enum_regions
-}
